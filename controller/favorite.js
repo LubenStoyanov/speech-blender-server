33ff518ead0c661076d74b26d405dbf3ae6854de
Fi <vancetti@googlemail.com>
2022-11-23 16:08:31 +0100

    add recording routing

---
+++ controller/favorite.js
@@ -0,0 +1,55 @@
+import connectDB from "../db.js";
+import { favorite } from "../models/favorite.js";
+
+export const createFavorite = async (req, res) => {
+  try {
+    connectDB();
+    const podcast = await Podcast.create(req.body);
+    res.status(201).json({ publicId: podcast.publicId });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+export const getPodcastsAll = async (req, res) => {
+  try {
+    connectDB();
+    const podcastsAll = await Podcast.find();
+    res.status(200).json(podcastsAll);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+export const getSearchPodcast = async (req, res) => {
+  try {
+    const { query } = req.params;
+    connectDB();
+    const searchPodcast = await Podcast.find({
+      title: { $regex: "^" + query },
+    });
+    res.status(200).json(searchPodcast);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+export const getDeletePodcast = async (req, res) => {
+  console.log("delete");
+  try {
+    const { publicId } = req.body;
+    console.log(publicId);
+    connectDB();
+    await Podcast.deleteOne({
+      publicId: publicId,
+    });
+
+    res.status(200).send("Podcast deleted");
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
---
+++ controller/recording.js
@@ -0,0 +1,28 @@
+import connectDB from "../db.js";
+import {
+    Recording
+} from "../models/recording.js";
+
+export const createRecording = async (req, res) => {
+    try {
+        connectDB();
+        const recording = await Recording.create(req.body);
+        res.status(201).json({
+            publicId: recording.publicId
+        });
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+};
+
+export const getRecordingsAll = async (req, res) => {
+    try {
+        connectDB();
+        const recordingsAll = await Recording.find();
+        res.status(200).json(recordingsAll);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+};
