c4a1e72fdbac4ec7cd537f14355b6319bcfaa1d4
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-11-23 17:45:00 +0100

    Setup AWS S3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-// import S3 from "aws-sdk/clients/s3";
 import helmet from "helmet";
 import cors from "cors";
 import validateToken from "./validateToken.js";
 import authRouter from "./routes/auth.js";
 import podcastRouter from "./routes/podcast.js";
+import { upload } from "./controller/upload.js";
 
 export const privateKey = process.env.PRIVATE_KEY;
 const app = express();
@@ -13,10 +13,11 @@ const port = process.env.PORT || 8080;
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", authRouter);
 app.use("/podcast", podcastRouter);
-
+app.use("/upload", upload);
 app.post("/profile/:username", validateToken, (req, res) => {
   console.log("Token is valid.");
   res.sendStatus(200);
