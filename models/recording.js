5c3efabe5796782a4d237df4e65c274aa0ae94b6
Fi <vancetti@googlemail.com>
2022-11-24 09:54:10 +0100

    add final recording routing

--- models/recording.js
+++ models/recording.js
@@ -1,18 +1,21 @@
-const recordingSchema = new Schema(
-  {
-    title: {
-      type: String,
-    },
-    url: {
-      type: String,
-      required: true,
-    },
-    userId: {
-      type: String,
-      required: true,
-      unique: 1,
-    },
+import mongoose, {
+  Schema
+} from "mongoose";
+
+const recordingSchema = new Schema({
+  title: {
+    type: String,
   },
-  { timestamps: true }
-);
+  url: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+    unique: 1,
+  },
+}, {
+  timestamps: true
+});
 export const Recording = mongoose.model("Recording", recordingSchema);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import validateToken from "./validateToken.js";
 import authRouter from "./routes/auth.js";
 import podcastRouter from "./routes/podcast.js";
+import recordingRouter from "./routes/recording.js";
 
 export const privateKey = process.env.PRIVATE_KEY;
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use("/", authRouter);
 app.use("/podcast", podcastRouter);
+app.use("/recording", recordingRouter);
 
 app.post("/profile/:username", validateToken, (req, res) => {
   console.log("Token is valid.");
