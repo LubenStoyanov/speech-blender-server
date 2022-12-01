de94028ab7140102675772c45e1d049105351f39
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-12-01 17:34:18 +0100

    Move cors up

--- server.js
+++ server.js
@@ -27,17 +27,15 @@ const app = express();
 const port = process.env.PORT || 8080;
 connectDB();
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// app.options("*", cors());
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", authRouter);
 app.use("/podcast", podcastRouter);
