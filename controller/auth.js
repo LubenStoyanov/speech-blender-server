9597efd2eac87cd6f00b2ac22c0cbac13d2339c2
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-12-07 11:44:34 +0100

    httpOnly false

--- controller/auth.js
+++ controller/auth.js
@@ -38,7 +38,7 @@ export const login = async (req, res) => {
 
     console.log("Setting cookie", token);
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: true,
       sameSite: "none",
     });
