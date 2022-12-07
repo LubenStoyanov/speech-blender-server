e9c9051fbc8d44d81eb297769db7ea1db011da8a
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-12-07 14:20:40 +0100

    No token middleware

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   if (token) {
     return next();
   } else {
-    return res.status(500).send("No token");
+    return res.status(200).send("No token");
   }
 });
 app.use("/", authRouter);
